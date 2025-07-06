3820a521339ece661d119e8441810a3d14e4d700
Grandiel <bgrandiel@gmail.com>
2025-07-06 04:24:54 -0400

    ergfweedg

--- voto.php
+++ voto.php
@@ -1,34 +1,39 @@
+<?php require 'config/i18n.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/styles/voto.css">
+    <link rel="shortcut icon" href="/Img/favicon.ico" type="image/x-icon" />
     <title><?= __('site_title') ?></title>
 </head>
 <body>
     <h1 class="title">Vota por tu favorito</h1>
 
-<div id="app" class="container">
-    <card data-image="https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=">
-        <h1 slot="header">Canyons</h1>
-        <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </card>
-    <card data-image="https://images.unsplash.com/photo-1479659929431-4342107adfc1?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=">
-        <h1 slot="header">Beaches</h1>
-        <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </card>
-    <card data-image="https://images.unsplash.com/photo-1479644025832-60dabb8be2a1?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=">
-        <h1 slot="header">Trees</h1>
-        <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </card>
-    <card data-image="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop=">
-        <h1 slot="header">Lakes</h1>
-        <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </card>
-</div>
+    <div id="app" class="container">
+        <card :data-image="'https://i.pinimg.com/736x/db/62/86/db628698b9e588926246c6309e30e588.jpg'">
+            <h1 slot="header">Mitología Eslava</h1>
+            <p slot="content">Bosques, Espíritus y Ancestros</p>
+        </card>
+        <card :data-image="'https://i.pinimg.com/736x/b1/e7/f3/b1e7f3aa3207248b933f488642d9c2d9.jpg'">
+            <h1 slot="header">Mitología Azteca</h1>
+            <p slot="content">Sacrificio, Guerra y Sol</p>
+        </card>
+        <card :data-image="'https://i.pinimg.com/736x/3d/6d/b2/3d6db2f75afdd70fcb8befc6ed7c2c75.jpg'">
+            <h1 slot="header">Hinduismo</h1>
+            <p slot="content">Karma, Dharma y Moksha</p>
+        </card>
+        <card :data-image="'https://i.pinimg.com/736x/ed/ae/fb/edaefbe97bdf09b5ba412bfc80574df7.jpg'">
+            <h1 slot="header">Mitología Japonesa</h1>
+            <p slot="content">Kami, Naturaleza y Armonía</p>
+        </card>
+    </div>
 
-<script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
-<script src="/JS/voto.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+    <script src="/JS/voto.js"></script>
 </body>
 </html>
